69086b784f6ba312bebc0cef8c69e662753cb8d2
runesrelics <brodiedenton@gmail.com>
2025-07-20 21:52:16 +0930

    Update main.py

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ class TicketActions(View):
 
     async def start_vouching(self, channel):
         user_list = list(self.users.values())
-        self.vouch_view = VouchView(self, channel, self.ticket_message, user_list[0], user_list[1], self.lister)
+        self.vouch_view = VouchView(self, channel, self.listing_message, user_list[0], user_list[1], self.lister)
         await cleanup_bot_messages(channel)
         await channel.send(f"{user_list[0].mention}, please rate your trade partner:", view=StarRatingView(self.vouch_view, user_list[0]))
         await channel.send(f"{user_list[1].mention}, please rate your trade partner:", view=StarRatingView(self.vouch_view, user_list[1]))
